e06958c05cb431b160fa9bbf5457fd554e64396f
Japsz <benjamin.meneses.14@sansano.usm.cl>
2021-08-10 18:21:20 -0400

    Trying GH Actions

--- front/src/pages/index.js
+++ front/src/pages/index.js
@@ -22,19 +22,30 @@ const IndexPage = () => (
           <p className="text-white h5">Full Stack Developer</p>
           <p className="text-white small">Curious and eager to learn</p>
         </div>
-        <div className="col mt-4">
+        <div className="col mt-4 d-flex align-items-center flex-column flex-sm-row">
           <StackOverflow/>
-          <a href="https://github.com/Japsz" className="btn btn-link ms-2">
+          <a href="https://github.com/Japsz" target="_blank" className="btn btn-white p-0 mt-2 mt-sm-0 d-flex ms-sm-2">
             <StaticImage
               src="../images/github-icon.png"
-              width={60}
+              width={58}
               quality={95}
+              className="me-2"
               formats={["AUTO", "WEBP", "AVIF"]}
               alt="Github Icon"
             />
-            Japsz
+            <span className="my-auto h4">@Japsz</span>
           </a>
         </div>
+      </div>
+    </div>
+    <div className="row">
+      <div className="container px-5 pt-5 d-flex flex-column align-items-start">
+        <h2>Hello</h2>
+        <p className="text-primary">
+          I'm Benjamín Meneses, born in Santiago, Chile. I've been doing FS Development since i learnt how to in 2015 while studying Computer Science at <a href="https://www.usm.cl/" target="_blank">UTFSM</a>.
+          <div className="mt-2"/>
+          My learning journey has always been towards improving how fast and memory-efficient my code is, as to finding new ways to make it consistent using different toolings and methods of Deployments.
+        </p>  
       </div>
     </div>
   </Layout>
