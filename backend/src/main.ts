48e8df790fb5c36c1924c32cc792a13edaa648ad
Japsz <benjamin.meneses.14@sansano.usm.cl>
2021-10-21 00:42:21 -0300

    Cors configuration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,17 +4,18 @@ import { AppModule } from './app.module';
 import * as admin from 'firebase-admin';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
   // Set the config options
-  const serviceAccount = process.env.FIREBASE_CREDENTIAL ?? require('./firebase-local-creds.json')
+  const serviceAccount = process.env.FIREBASE_CREDENTIAL ?? require('./firebase-credentials.json')
   // Initialize the firebase admin app
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://gatsby-portfolio-c8fd3-default-rtdb.firebaseio.com",
   });
-  app.enableCors({
-    origin: ['http://localhost:8000', 'https://japsz.github.io', 'https://www.bmeneses.io']
-  })
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: ['http://localhost:8000', 'https://japsz.github.io', /\.bmeneses\.io$/]
+    }
+  });
   await app.listen(3000);
 }
 bootstrap();
