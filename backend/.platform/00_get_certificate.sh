90af6d35e231f712eca8dc81604fc3ed9732953a
Japsz <benjamin.meneses.14@sansano.usm.cl>
2021-10-21 00:08:48 -0300

    Second Attempt of SSL LetsEncrypt

--- backend/.platform/00_get_certificate.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-sudo certbot -n -d api.bmeneses.io --nginx --agree-tos --email apuzito@gmail.com
---
+++ backend/.platform/hooks/postdeploy/00_get_certificate.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+sudo certbot --nginx --standalone --debug --non-interactive --email apuzito@gmail.com --agree-tos -d api.bmeneses.io
